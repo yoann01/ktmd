43d7304cad9155340d943a9d242f36b57c892f09
yoann <ygranier@gmail.com>
2017-08-24 01:30:38 +0200

    Update youtube.rb

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,28 +1,77 @@
-class YouTube < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+require 'cgi'
+require 'uri'
 
-  def initialize(tagName, markup, tokens)
-    super
+EMBED = %(<style>.embed-container { position: relative; padding-bottom: 56.25%%; height: 0; overflow: hidden; max-width: 100%%; height: auto; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%%; height: 100%%; }</style><div class='embed-container'>%{video}</div>)
 
-    if markup =~ Syntax then
-      @id = $1
+VIMEO = %(<iframe src="%{scheme}://player.vimeo.com/video/%{video_id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>)
 
-      if $2.nil? then
-          @width = 560
-          @height = 420
-      else
-          @width = $2.to_i
-          @height = $3.to_i
+YOUTUBE = %(<iframe src="%{scheme}://www.youtube.com/embed/%{video_id}/" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>)
+
+module Jekyll
+
+  class ResponsiveEmbedConverter < Converter
+    safe true
+    priority :highest
+    @@domains = %w(youtube.com vimeo.com)
+
+    def matches(ext)
+      ext =~ /^\.md$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def interesting_domain(href)
+      @@domains.any? { |s| href.include?(s) }
+    end
+
+    def interesting_url(href)
+      interesting_domain(href) && href.include?('jekyll_embed')
+    end
+
+    def get_embed_markup(url, params)
+      # the URL has a jekyll_embed GET paramter
+      context = {
+        scheme: url.scheme
+      }
+      unless ENV["RESPONSIVE_EMBED_SCHEME"].nil?
+        context['scheme'] = ENV["RESPONSIVE_EMBED_SCHEME"]
       end
-    else
-      raise "No YouTube ID provided in the \"youtube\" tag"
+
+      if url.host.include?('youtube.com')
+        # get the YouTube iframe
+        context[:video_id] = params['v'][0]
+        video = YOUTUBE % context
+      elsif url.host.include?('vimeo.com')
+        # get the Vimeo iframe
+        context[:video_id] = url.path[1..-1]
+        video = VIMEO % context
+      end
+      unless video.nil?
+        embed = EMBED % {video: video}
+      end
+    end
+
+    def convert(content)
+      re = %r{(\n    )?(\[.*\])\((http.*)\)}
+      m = content.scan re
+      m.each do |match|
+        next unless match[0].nil?
+        href = match[2]
+        if href and interesting_url(href)
+          url = URI(href)
+          params = CGI.parse(url.query)
+          if params.key?('jekyll_embed')
+            embed = get_embed_markup(url, params)
+            # if we have some embed markup replace the Markdown link with it
+            if embed
+              content = content.gsub("#{match[1]}(#{match[2]})", embed)
+            end
+          end
+        end
+      end
+      content
     end
   end
-
-  def render(context)
-    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
-  end
-
-  Liquid::Template.register_tag "youtube", self
 end
